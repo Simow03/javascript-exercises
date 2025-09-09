e754ef3b2f4cc4e0d41adfc70e9160c5bcf93927
mohamed staali <imthetigerz@gmail.com>
2025-09-09 01:22:27 +0100

    solve array and loops exercises

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+        return "ERROR"
+    let result = 0;
+    if (a < b) {
+        for(let i = a; i <= b; i++) {
+            result += i;
+        }
+    }
+    else if (a > b) {
+        for(let i = b; i <= a; i++) {
+            result += i;
+        }
+    }
+    else return a;
 
+    return result;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(FahrenheitValue) {
+  return parseFloat(((FahrenheitValue - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(CelsiusValue) {
+  return parseFloat(((CelsiusValue * 9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
